a1f08d6cc2112c387bcc14f32c9ffddcbcf7bea6
Khari <khari.lane@khalikenterprises.com>
2019-06-17 12:33:30 -0500

    Database schema rough draft

--- db/index.js
+++ db/index.js
@@ -1,1 +1,44 @@
 // database index
+const Sequelize = require('sequelize');
+
+const dbName = process.env.DATABASE_NAME || "Burboun";
+const uName = process.env.USERNAME || "OmarScrumMaster";
+const pw = process.env.PASSWORD || "";
+const host = process.env.HOST || ""; // find correct value for host
+const port = process.env.PORT || 8080;
+
+const sequelize = new Sequelize(dbName, uName, pw, {
+  host,
+  port,
+  dialect: 'mysql',
+});
+
+const User = sequelize.define('user', { // model schema for user -- lowercase for psql.
+  name: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  profilePic: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+const Location = sequelize.define('location', {
+  name: Sequelize.STRING,
+  rating_avg: Sequelize.INTEGER,
+
+})
+
+const Ratings = sequelize.define('ratings', {
+  rating: Sequelize.INTEGER,
+}); // create join table as new table so it can be referenced as variable
+Ratings.belongsTo(User); // define join table relationship to User
+Ratings.belongsTo(Location);
+
+const Users_Locations = sequelize.define('users_locations', {}); // create join table as new table so it can be referenced as variable
+Users_Locations.belongsTo(User); // define join table relationship to User
+Users_Locations.belongsTo(Location);
+
+const Comment = sequelize.define('comment', {
+  text: Sequelize.STRING,
+}); // create join table as new table so it can be referenced as variable
+Comment.belongsTo(User); // define join table relationship to User
+Comment.belongsTo(Location);
+
