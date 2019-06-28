f754211b42b0e9e1dab01c5c6d0f4667af4fd656
Stephen Guillory <stepheng1492@gmail.com>
2019-06-28 16:47:46 -0500

    mass text capability

--- db/dbHelpers/helpers.js
+++ db/dbHelpers/helpers.js
@@ -30,9 +30,7 @@ const saveLocation = location => Location.create({
   lat: location.lat,
 });
 
-const bulkLocations = (locations) => {
-  return Location.bulkCreate(locations);
-};
+const bulkLocations = locations => Location.bulkCreate(locations);
 
 const getLocID = location => Location.findOne({
   where: {
@@ -58,12 +56,10 @@ const downRating = locationId => Ratings.create({
   locationId,
 });
 
-<<<<<<< HEAD
 const addGuardian = guardian => Guardians.create({
   name: guardian.name,
   number: guardian.phone,
 });
-=======
 const diffHours = (t2, t1) => {
   let diff = (t2.getTime() - t1.getTime()) / 1000;
   diff /= (60 * 60);
@@ -95,9 +91,7 @@ const updateLocationRatingAvg = (locationId) => {
         where: { id: locationId },
       });
     });
-
 };
->>>>>>> 8efa1b4295b9e67fa2ee8525c3b3f7d53912239b
 
 module.exports = {
   signUpUser,
@@ -108,9 +102,6 @@ module.exports = {
   getComments,
   postRating,
   downRating,
-<<<<<<< HEAD
   addGuardian,
-=======
-  updateLocationRatingAvg
->>>>>>> 8efa1b4295b9e67fa2ee8525c3b3f7d53912239b
+  updateLocationRatingAvg,
 };
